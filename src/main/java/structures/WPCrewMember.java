784a3a98baf35fab616a67b4fce151df72f5fad4
Neil Martey-Botchway <neil.marteybotchway@gmail.com>
2025-04-28 10:51:37 +0100

    Comment for crewMember

--- src/main/java/structures/WPCrewMember.java
+++ src/main/java/structures/WPCrewMember.java
@@ -15,31 +15,66 @@ public class WPCrewMember {
         this.films = new WPArrayList<Integer>();
     }
 
-    // Adds a film to the crew member's list, if not already added
+    /**
+     * Adds a film to the crew member's list of films, if it is not already included.
+     * 
+     * This method checks whether the specified {@code filmID} is already in the crew member's list of films.
+     * If the film is not already associated with the crew member, it is added to the list.
+     * 
+     * @param filmID The ID of the film to be added.
+     */
     public void addFilm(int filmID) {
         if (!this.films.contains(filmID)) {
             this.films.add(filmID);
         }
     }
 
-    // Removes a film from the crew member's list, if it exists
+    /**
+     * Removes a film from the crew member's list of films, if it exists.
+     * 
+     * This method checks whether the specified {@code filmID} is present in the crew member's list of films.
+     * If the film exists, it is removed from the list.
+     * 
+     * @param filmID The ID of the film to be removed.
+     */
     public void removeFilm(int filmID) {
         if (this.films.contains(filmID)) {
             this.films.remove(filmID);
         }
     }
 
-    // Checks if the crew member is not associated with any films
+    /**
+     * Checks if the crew member is not associated with any films.
+     * 
+     * This method returns {@code true} if the crew member has no films associated with them, 
+     * and {@code false} otherwise.
+     * 
+     * @return {@code true} if the crew member has no films, {@code false} otherwise.
+     */
     public boolean emptyFilms() {
         return this.films.size() == 0;
     }
-    
-    // Returns the Person object representing this crew member
+
+    /**
+     * Returns the {@code Person} object representing this crew member.
+     * 
+     * This method retrieves the {@code Person} associated with the crew member, 
+     * which provides details about the crew member's personal information.
+     * 
+     * @return The {@code Person} object representing the crew member.
+     */
     public Person getPerson() {
         return this.person;
     }
 
-    // Returns an array of film IDs that the crew member is associated with
+    /**
+     * Returns an array of film IDs that the crew member is associated with.
+     * 
+     * This method creates an array of {@code int} containing the film IDs 
+     * representing the films the crew member has worked on.
+     * 
+     * @return An array of film IDs associated with the crew member.
+     */
     public int[] getFilms() {
         int[] castedStarredFilms = new int[this.films.size()];
         for (int i = 0; i < this.films.size(); i++) {
@@ -47,4 +82,5 @@ public class WPCrewMember {
         }
         return castedStarredFilms;
     }
+
 }
